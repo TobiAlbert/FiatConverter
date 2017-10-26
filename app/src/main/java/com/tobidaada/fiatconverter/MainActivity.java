f3d63771ec73419571a5b5fb316852b1670918b0
Tobi Daada <daadatobi@gmail.com>
2017-10-26 13:25:25 +0100

    Implement onLongClickListener method in RecyclerView

--- app/src/main/java/com/tobidaada/fiatconverter/MainActivity.java
+++ app/src/main/java/com/tobidaada/fiatconverter/MainActivity.java
@@ -9,16 +9,16 @@ import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-
+import android.widget.Toast;
 
 import com.tobidaada.fiatconverter.adapter.RVAdapter;
 import com.tobidaada.fiatconverter.adapter.RVTouchListener;
 import com.tobidaada.fiatconverter.model.data.Card;
-
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,13 +43,11 @@ public class MainActivity extends AppCompatActivity {
 
         mCardList = new ArrayList<>();
 
-
         adapter = new RVAdapter(mCardList, this);
 
         mRecyclerView.setAdapter(adapter);
 
         initializeCard();
-
 
         mRecyclerView.addOnItemTouchListener(new RVTouchListener(getApplicationContext(), mRecyclerView,
                 new RVTouchListener.ClickListener() {
@@ -58,10 +56,41 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(View view, int position) {
 
                     }
+
+                    @Override
+                    public void onLongClick(View view, int position) {
+
+                        final int cardPosition = position;
+
+                        AlertDialog.Builder mAlertDialog = new AlertDialog.Builder(view.getContext());
+                        mAlertDialog.setMessage(R.string.delete_card)
+                                .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int position) {
+
+                                        position = cardPosition;
+
+                                        mCardList.remove(position);
+                                        adapter.notifyItemRemoved(position);
+                                        dialog.dismiss();
+                                    }
+
+                                })
+                                .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+
+                                    @Override
+                                    public void onClick(DialogInterface dialog , int position) {
+                                        dialog.dismiss();
+                                    }
+
+                                });
+
+                        mAlertDialog.create().show();
+                    }
                 }));
 
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -110,7 +139,7 @@ public class MainActivity extends AppCompatActivity {
                                     @Override
                                     public void onClick(View v) {
                                         mCardList.remove(mCardList.size() - 1);
-                                        adapter.notifyItemRemoved(mCardList.size() - 1);
+                                        adapter.notifyItemRemoved(mCardList.size());
                                     }
 
                                 })
@@ -123,11 +152,9 @@ public class MainActivity extends AppCompatActivity {
         mAlertDialog.create().show();
     }
 
-
     public void initializeCard() {
         mCardList.add( new Card("Bitcoin", ""));
         //mCardList.add(new Card("Ethereum", ""));
     }
-
 
 }
