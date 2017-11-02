2d9fee9998a34416c30411de468378e5089ed52d
Tobi Daada <daadatobi@gmail.com>
2017-11-02 14:16:19 +0100

    Implement ConversionMvpContract.View interface

--- app/src/main/java/com/tobidaada/fiatconverter/ui/conversion/ConversionActivity.java
+++ app/src/main/java/com/tobidaada/fiatconverter/ui/conversion/ConversionActivity.java
@@ -16,7 +16,7 @@ import com.tobidaada.fiatconverter.model.data.CurrencyTable;
 
 import java.util.Locale;
 
-public class ConversionActivity extends AppCompatActivity {
+public class ConversionActivity extends AppCompatActivity implements ConversionMvpContract.View {
 
     TextView fiatCurrencyText;
     EditText fiatCurrencyEditText;
@@ -24,11 +24,14 @@ public class ConversionActivity extends AppCompatActivity {
     TextView cryptoValue;
     Button conversionButton;
     Intent getIntent;
+    ConversionMvpContract.Presenter contractPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversion);
+
+        contractPresenter = new ConversionPresenter(this);
 
         getIntent = getIntent();
 
@@ -39,15 +42,8 @@ public class ConversionActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                String conversionValue = getIntent.getStringExtra("conversionValue");
-
-                double fiatCurrency = Double.parseDouble(fiatCurrencyEditText.getText().toString());
-                double conversionValueToDouble = Double.parseDouble(conversionValue);
-
-                double amount = fiatCurrency / conversionValueToDouble;
-
-                cryptoValue.setText(String.format(Locale.getDefault(), "%,.5f", amount));
-
+                contractPresenter.convertCurrency(getIntent.getStringExtra("conversionValue"),
+                        fiatCurrencyEditText.getText().toString());
             }
         });
     }
@@ -75,5 +71,10 @@ public class ConversionActivity extends AppCompatActivity {
         fiatCurrencyText.setText(CurrencyTable.getTickerSymbol(fiatCurrency));
         cryptoText.setText(CurrencyTable.getTickerSymbol(cryptoCurrency));
 
+    }
+
+    @Override
+    public void showConversion(String result) {
+        cryptoValue.setText(result);
     }
 }
