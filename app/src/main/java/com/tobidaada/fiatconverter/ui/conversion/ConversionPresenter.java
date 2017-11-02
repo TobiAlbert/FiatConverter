e29c1eb3eb309f1dea3e879f49cf0aae2ef4ec47
Tobi Daada <daadatobi@gmail.com>
2017-11-02 14:17:23 +0100

    Implement ConversionMvpContract.Presenter interface

--- app/src/main/java/com/tobidaada/fiatconverter/ui/conversion/ConversionPresenter.java
+++ app/src/main/java/com/tobidaada/fiatconverter/ui/conversion/ConversionPresenter.java
@@ -1,8 +1,28 @@
 package com.tobidaada.fiatconverter.ui.conversion;
 
+import java.util.Locale;
+
 /**
  * Created by TOBI DAADA on 11/2/2017.
  */
 
-public class ConversionPresenter {
+public class ConversionPresenter implements ConversionMvpContract.Presenter {
+
+    private ConversionMvpContract.View contractView;
+
+    public ConversionPresenter(ConversionMvpContract.View contractView) {
+        this.contractView = contractView;
+    }
+
+
+    @Override
+    public void convertCurrency(String currencyConversion, String userInput) {
+
+        double currencyConversionDouble = Double.parseDouble(currencyConversion);
+        double userInputDouble = Double.parseDouble(userInput);
+
+        double amount = userInputDouble / currencyConversionDouble;
+
+        contractView.showConversion(String.format(Locale.getDefault(), "%,.5f", amount));
+    }
 }
